895dc153806bd94fef2fae043460755229a47769
alex2000d <demarcoalessandro777@gmail.com>
2024-06-26 19:15:47 +0200

    [UPD] created my ticket

--- js/script.js
+++ js/script.js
@@ -6,31 +6,32 @@ let btn = document.getElementById('generator');
 btn.addEventListener('click', function (){
   let name = document.getElementById('name').value
   let km = document.getElementById('km').value
-  let eta = document.getElementById('age').value
+  let age = document.getElementById('age').value
 
-  
   let prezzo_biglietto = 0.21 * km;
   console.log(prezzo_biglietto);
-  let prezzo_finale = prezzo_totale
+  let prezzo_finale = prezzo_biglietto
   let message = prezzo_finale;
-  console.log(prezzo_finale);
-  if (age < 18) {
+  console.log(age);
+  if (age === 'minorenne') {
     // se il passeggero e minorenne applico uno sconto del 20%
     let sconto = prezzo_biglietto * 20 / 100;
     prezzo_finale = prezzo_finale - sconto;
-    let message = message + prezzo_finale;
+    message = message + prezzo_finale;
 
 }
-else if (age > 65) {
+else if (age === 'over') {
     // se il passeggero è over 65 applico uno sconto del 40%
     let sconto = prezzo_biglietto * 40 / 100;
     prezzo_finale = prezzo_finale - sconto;
-    let message = message + prezzo_finale;
+    message = message + prezzo_finale;
 
 }
+console.log(prezzo_finale);
 })
 
-
+ document.getElementById('message').innerHTML = `<h4>${name}<h4>`;
+ 
 
 
 
